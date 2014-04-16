2a6cf1801da79e7f7aae8d2bf86aa2d501852c96
shibingwen <shibingwen@douban.com>
2014-04-16 15:55:21 +0800

    koa demo update

--- node_bingbing/node5_koa_app.js
+++ node_bingbing/node5_koa_app.js
@@ -1,5 +1,20 @@
 var koa = require('koa'), app = koa();
 
+app.use(function *(next){
+  var start = new Date;
+  yield next;
+  var ms = new Date - start;
+  this.set('X-Response-Time', ms + 'ms');
+  console.log('X-Response-Time - %s', this.method, this.url, ms);
+});
+
+app.use(function *(next){
+  var start = new Date;
+  yield next;
+  var ms = new Date - start;
+  console.log('%s %s - %s', this.method, this.url, ms);
+});
+
 app.use(function*(){
     this.body = 'Hello Koa';
 });
